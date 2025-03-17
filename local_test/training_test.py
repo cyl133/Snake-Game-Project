7b5ad3b1c9996bc1425766f7260d633994a99ffe
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-17 08:58:47 +0800

    train

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -64,8 +64,8 @@ from gym_env import SnakeGameEnv
 
 env = SnakeGameEnv(num_snakes=1, num_teams=1)
 
-model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', n_steps=64)
-# model = PPO.load("ppo_snake", env=env, device="cuda")
+# model = PPO('MultiInputPolicy', env, verbose=True, device='cuda')
+model = PPO.load("ppo_snake", env=env, device="cuda")
 
-model.learn(100000, progress_bar=True)
+model.learn(10000, progress_bar=True)
 model.save('ppo_snake')

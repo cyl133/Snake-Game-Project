65909944c3cc9e19f39c7308faf30bbc1831d7c3
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-21 00:40:42 +0800

    added action probability checking during evaluation

--- local_test/eval.py
+++ local_test/eval.py
@@ -1,15 +1,41 @@
 from stable_baselines3.common.evaluation import evaluate_policy
 from gym_env import SnakeGameEnv
 from stable_baselines3 import PPO
+import torch
 
 
 # Load the trained model
-model = PPO.load("models/ppo_snake3.112.zip")
+model = PPO.load("models/ppo_snake3.1-1mil.zip")
 
 # Create a new environment instance for evaluation
 env = SnakeGameEnv(num_snakes=1, num_teams=1, render_mode='human')
 
 # Evaluate the model
-rew, std = evaluate_policy(model, env, n_eval_episodes=50, render=True, return_episode_rewards=False, warn=False)
+# rew, std = evaluate_policy(model, env, n_eval_episodes=50, render=True, return_episode_rewards=False, warn=False)
 
-print(f"Mean Reward: {rew:.2f}, Std Reward: {std:.2f}")
+
+# For getting the explicit actions probabilities, could be good for data and reporting
+num_episodes = 10
+all_action_probs = []
+
+for _ in range(num_episodes):
+    obs, _ = env.reset()
+    done = False
+
+    while not done:
+        with torch.no_grad():
+            tensor_obs, _ = model.policy.obs_to_tensor(obs)
+            action_dist = model.policy.get_distribution(tensor_obs)
+            action_probs = torch.exp(action_dist.distribution.logits).round(decimals=3)
+            all_action_probs.append(action_probs.cpu().numpy())
+
+        print(f"Action Dist: {action_probs}")
+        env.render()
+        action, _ = model.predict(obs, deterministic=False)
+        print(f"Action: {action}")
+        obs, reward, done,_, _ = env.step(int(action))
+
+
+
+
+# print(f"Mean Reward: {rew:.2f}, Std Reward: {std:.2f}")

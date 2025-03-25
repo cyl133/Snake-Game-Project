c7beed675a0e794eeedecd8aa252060e9c5dcded
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-25 22:36:04 +0800

    3.13 test 2

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -14,10 +14,10 @@ log_dir = "logs"
 
 env = SnakeGameEnv(num_snakes=1, num_teams=1)
 
-model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=64)
+model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=1024)
 # model = PPO.load("ppo_snake", env=env, device="cuda", tensorboard_log=log_dir)
 
 
-for i in range(1):
-    model.learn(64, progress_bar=True, tb_log_name="test", reset_num_timesteps=False)
-    model.save('test.zip')
+for i in range(10):
+    model.learn(10000, progress_bar=True, tb_log_name="PPO-3.13", reset_num_timesteps=False)
+    model.save('PPO-3.13.zip')

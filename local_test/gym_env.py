6fed21408c54bb780375107986ec76ad9cb96816
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-26 10:14:46 +0800

    3.131 1 mil

--- local_test/gym_env.py
+++ local_test/gym_env.py
@@ -14,7 +14,7 @@ MAX_FRUITS = 10
 
 # Rewards
 reward_map = {
-    SnakeState.OK: 0,
+    SnakeState.OK: -1,
     SnakeState.ATE: 5,
     SnakeState.DED: -10,
     SnakeState.WON: 1
--- local_test/training_test.py
+++ local_test/training_test.py
@@ -1,23 +1,18 @@
-import torch as th
-import torch.nn as nn
-from gymnasium import spaces
-
 from stable_baselines3 import PPO
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.env_util import make_vec_env
 from gym_env import SnakeGameEnv
-import time
 
 
 log_dir = "logs"
 
+vec_env = make_vec_env(lambda: SnakeGameEnv(num_snakes=1, num_teams=1), n_envs=4)
+# env = SnakeGameEnv(num_snakes=1, num_teams=1)
 
-env = SnakeGameEnv(num_snakes=1, num_teams=1)
 
-model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=1024)
-# model = PPO.load("ppo_snake", env=env, device="cuda", tensorboard_log=log_dir)
+model = PPO('MultiInputPolicy', vec_env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=1024)
+# # model = PPO.load("ppo_snake", env=env, device="cuda", tensorboard_log=log_dir)
 
 
 for i in range(10):
-    model.learn(100000, progress_bar=True, tb_log_name="PPO-3.13-1mil", reset_num_timesteps=False)
-    model.save('PPO-3.13-1mil.zip')
+    model.learn(100000, progress_bar=True, tb_log_name="PPO-3.131-1mil", reset_num_timesteps=False)
+    model.save('PPO-3.131-1mil.zip')

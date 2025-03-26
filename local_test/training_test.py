d61b2b4e23cb1ba6c7dd60143382f1b635339fa4
Breaddcrumbss <lukechong2898.lc@gmail.com>
2025-03-26 13:56:10 +0800

    change lr decays after 0.3 timesteps

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -3,13 +3,29 @@ from stable_baselines3.common.env_util import make_vec_env
 from gym_env import SnakeGameEnv
 
 
+class LRSchedule:
+    def __init__(self, total_timesteps, decay_start=0.5):
+        self.total_timesteps = total_timesteps
+        self.decay_start = decay_start
+        self.timesteps_trained = 0
+
+    def __call__(self, _):
+        progress_remaining = 1 - self.timesteps_trained / self.total_timesteps
+
+        if progress_remaining < self.decay_start:
+            return 3e-4 * (progress_remaining / self.decay_start)
+        return 3e-4 * progress_remaining
+
+    def update(self, train_steps):
+        self.timesteps_trained += train_steps
+
 log_dir = "logs"
 
 # vec_env = make_vec_env(lambda: SnakeGameEnv(num_snakes=1, num_teams=1), n_envs=2)
 env = SnakeGameEnv(num_snakes=1, num_teams=1)
 
 
-model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=1024, learning_rate=lambda x: 0.003*x)
+model = PPO('MultiInputPolicy', env, verbose=True, device='cuda', tensorboard_log=log_dir, n_steps=1024, learning_rate=LRSchedule(1e6, 0.3))
 # # model = PPO.load("ppo_snake", env=env, device="cuda", tensorboard_log=log_dir)
 
 

a1569ece5fcbc0a0def922b875a14b5903b52d9f
cyli <your.email@example.com>
2025-04-13 15:38:17 +0800

    food only reward

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -21,7 +21,7 @@ CONFIG_DIR = "param_configs"
 LOG_DIR = "logs_wandb"
 MODEL_DIR = "models_wandb"
 LLM_CALL_FREQUENCY = 30000  # Episodes before LLM update
-METRICS_LOG_FREQUENCY = 512
+METRICS_LOG_FREQUENCY = 1024
 N_ENVS = 128  # Number of environments
 USE_LLM = False  # SET THIS TO FALSE TO DISABLE LLM COMPLETELY
 

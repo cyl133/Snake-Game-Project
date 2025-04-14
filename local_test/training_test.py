c7d73de2681eb5ff4fc5414227ec69e8bbbd9eec
cyli <your.email@example.com>
2025-04-15 02:08:36 +0800

    0.05 ent

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -500,7 +500,7 @@ def train():
         "n_steps": 128,
         "batch_size": 2048,
         "gae_lambda": 0.99,
-        "ent_coef": 0.2
+        "ent_coef": 0.05
     }
 
     # --- Load or Create Model ---

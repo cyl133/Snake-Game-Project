1ac514ee8d359ae07fca5ebb3d5bf70230ba4475
cyli <your.email@example.com>
2025-04-15 02:12:57 +0800

    train for 50mil

--- local_test/training_test.py
+++ local_test/training_test.py
@@ -438,7 +438,7 @@ def train():
         project="snake-rl-simple",
         config={
             "policy_type": "CnnPolicy",
-            "total_timesteps": 5_000_000,
+            "total_timesteps": 5_000_0000,
             "n_envs": N_ENVS,
             "learning_rate": 3e-4,
             "n_steps": 128,
@@ -552,7 +552,7 @@ def train():
     # --- Train ---
     try:
         # Determine total timesteps remaining if loading a model
-        total_timesteps_config = run.config.get("total_timesteps", 5_000_0000)
+        total_timesteps_config = run.config.get("total_timesteps", 5_000_000)
         timesteps_to_train = total_timesteps_config - model.num_timesteps
         if timesteps_to_train <= 0:
              print("Model already trained for total timesteps. Exiting.")
